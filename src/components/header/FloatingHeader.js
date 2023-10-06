5ced91a160818506315f30841755ea1ed17f90f1
mohammadj789 <mohammadsoltanian10@gmail.com>
2023-10-06 22:43:55 +0330

    fix floating

--- src/components/header/FloatingHeader.js
+++ src/components/header/FloatingHeader.js
@@ -1,7 +1,7 @@
 export function FloatingHeader(props) {
   return (
-    <div className="w-full h-12 bg-transparent absolute flex justify-between p-4">
-      <div className="flex items-center gap-2 pt-5">
+    <div className="w-full bg-transparent absolute flex justify-between p-2">
+      <div className="flex items-center gap-2">
         <button
           onClick={() => props.setPlayList((prev) => !prev)}
           className="flex items-center justify-center bg-black bg-opacity-30 text-white rounded-full h-9 w-9 "
@@ -27,7 +27,7 @@ export function FloatingHeader(props) {
           ></ion-icon>
         </button>
       </div>
-      <button>
+      <button className="h-fit">
         <img
           src="/girl.jpg"
           className="w-10 h-10 object-cover rounded-full border-[6px] border-zinc-800 border-opacity-80"
